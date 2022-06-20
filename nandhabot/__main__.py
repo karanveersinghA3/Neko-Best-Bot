1142a1b94f09e1513597551fde4853edae283319
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+Ctzfamily@users.noreply.github.com>
2022-06-21 01:38:15 +0530

    Update __main__.py

--- nandhabot/__main__.py
+++ nandhabot/__main__.py
@@ -1,9 +1,6 @@
 import os
-import logging
 import pyrogram
 
-logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
-                    level=logging.WARNING)
 
 API_ID = os.environ.get("API_ID", None)
 API_HASH = os.environ.get("API_HASH", None)
@@ -11,17 +8,7 @@ BOT_TOKEN = os.environ.get("BOT_TOKEN", None)
 SUPPORT = os.environ.get("SUPPORT", None)
 UPDATES = os.environ.get("UPDATES", None)
 
-if __name__ == "__main__" :
-    print("Starting Bot...")
-    plugins = dict(root="nandhabot/plugins")
-    bot = pyrogram.Client(
-        "nandhabot",
-        bot_token=BOT_TOKEN,
-        api_id=API_ID,
-        api_hash=API_HASH,
-        plugins=plugins
-    )
-    bot.run()
+bot = Client("nandhabot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN, plugins=dict(root="{}/plugins".format(__name__)))
 
 
 PM_START_TEXT = "hello! {}\nI'm anime themed nekos bot below click the help button know my commandslist!"
