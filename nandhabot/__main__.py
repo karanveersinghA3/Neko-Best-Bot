0ed315cff50923039b844993f56e57e6898198ef
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+Ctzfamily@users.noreply.github.com>
2022-06-21 00:26:58 +0530

    Update __main__.py

--- nandhabot/__main__.py
+++ nandhabot/__main__.py
@@ -5,6 +5,12 @@ import pyrogram
 logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
                     level=logging.WARNING)
 
+API_ID = os.environ.get("API_ID", None)
+API_HASH = os.environ.get("API_HASH", None)
+BOT_TOKEN = os.environ.get("BOT_TOKEN", None)
+SUPPORT = os.environ.get("SUPPORT", None)
+UPDATES = os.environ.get("UPDATES", None)
+
 if __name__ == "__main__" :
     print("Starting Bot...")
     plugins = dict(root="nandhabot/plugins")
