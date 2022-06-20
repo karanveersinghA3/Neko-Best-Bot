1a4d69b57f630356f27a84e891e60daafdb14adf
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+Ctzfamily@users.noreply.github.com>
2022-06-21 01:14:35 +0530

    Update start.py

--- nandhabot/plugins/start.py
+++ nandhabot/plugins/start.py
@@ -2,8 +2,10 @@ import random
 from nandhabot import bot
 from pyrogram import filters
 from pyrogram.types import *
-from nandhabot import PM_START_TEXT, PM_START_IMG, UPDATES, SUPPORT, BOT_USERNAME
+from nandhabot import PM_START_TEXT, PM_START_IMG, UPDATES, SUPPORT
 
+x = bot.get_me()
+BOT_USERNAME = x.username
 
 buttons = [[
             InlineKeyboardButton("ADD ME", url=f"t.me/{BOT_USERNAME}?startgroup=true"),
