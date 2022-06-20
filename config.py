9254e09ff17de1ae586a38e56ee19235c0695d2d
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+Ctzfamily@users.noreply.github.com>
2022-06-21 00:24:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,7 @@
-import os
-from os import getenv
 
-API_ID = int(os.getenv("API_ID"))
-API_HASH = os.getenv("API_HASH")
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-SUPPORT_CHAT = os.getenv("SUPPORT_CHAT")
-UPDATES_CHANNEL = os.getenv("UPDATES_CHANNEL")
+
+API_ID = ""
+API_HASH = ""
+BOT_TOKEN = ""
+SUPPORT_CHAT = ""
+UPDATES_CHANNEL = ""
