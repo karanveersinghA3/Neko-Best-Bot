ac09bc629174001054653df2f4f2a4156d7c8b1e
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+Ctzfamily@users.noreply.github.com>
2022-06-21 00:42:04 +0530

    Update start.py

--- nandhabot/plugins/start.py
+++ nandhabot/plugins/start.py
@@ -3,7 +3,15 @@ from pyrogram import filters
 from pyrogram.types import *
 from nandhabot import PM_START_TEXT
 
+
+buttons = [[
+            InlineKeyboardButton("ADD ME", url=f"t.me/{BOT_USERNAME}?startgroup=true"),
+            InlineKeyboardButton("HELP", callback_data='help_back'),
+           ],[
+            InlineKeyboardButton("SUPPORT", url=f"https://t.me/{SUPPORT}"),
+            InlineKeyboardButton("UPDATES", url=f"https://t.me/{UPDATES}")]]
+
 @bot.on_message(filters.command("start"))
 async def (_, m):
-        await m.reply_text(PM_START_TEXT)
+        await m.reply_text(PM_START_TEXT,)
    
