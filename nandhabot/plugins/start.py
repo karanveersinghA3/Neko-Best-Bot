e40cc39e13241759e6e3e5ad07004f7e3aabd1dc
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+Ctzfamily@users.noreply.github.com>
2022-06-21 00:47:21 +0530

    Update start.py

--- nandhabot/plugins/start.py
+++ nandhabot/plugins/start.py
@@ -6,7 +6,7 @@ from nandhabot import PM_START_TEXT, PM_START_IMG
 
 buttons = [[
             InlineKeyboardButton("ADD ME", url=f"t.me/{BOT_USERNAME}?startgroup=true"),
-            InlineKeyboardButton("HELP", callback_data='help_back'),
+            InlineKeyboardButton("HELP", callback_data="help_back"),
            ],[
             InlineKeyboardButton("SUPPORT", url=f"https://t.me/{SUPPORT}"),
             InlineKeyboardButton("UPDATES", url=f"https://t.me/{UPDATES}")]]
@@ -14,5 +14,10 @@ buttons = [[
 @bot.on_message(filters.command("start"))
 async def (_, m):
         await m.reply_photo(photo=PM_START_IMG,caption=PM_START_TEXT,
-reply_markup=InlineKeyboardMarkup(buttons))
+             reply_markup=InlineKeyboardMarkup(buttons))
    
+
+@bot.on_callback_query(filters.regex("help_back"))
+async def close(_, query: CallbackQuery):
+           query = query.message
+           await query.edit(HELP_TEXT)
