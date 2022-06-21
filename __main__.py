73e2df51a7f083c019b32e8ffe97d337295115ba
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+Ctzfamily@users.noreply.github.com>
2022-06-21 07:29:25 +0530

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -18,7 +18,7 @@ BOT_TOKEN = os.environ.get("BOT_TOKEN", None)
 SUPPORT = os.environ.get("SUPPORT", None)
 UPDATES = os.environ.get("UPDATES", None)
 BOT_USERNAME = os.environ.get("BOT_USERNAME", None) 
-PM_START_IMG = os.environ.get("PM_START_IMG" , "https://telegra.ph/file/895f87f9f8028ecc4b632.jpg")
+PM_START_IMG = os.environ.get("PM_START_IMG" , None)
 PM_START_TEXT = os.environ.get("PM_START_TEXT", None) 
 
 bot = Client("nandhabot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
