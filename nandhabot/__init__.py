335c96634a35d19adb4263ad110ea8fa207254be
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+Ctzfamily@users.noreply.github.com>
2022-06-21 06:54:05 +0530

    Update __init__.py

--- nandhabot/__init__.py
+++ nandhabot/__init__.py
@@ -7,14 +7,9 @@ BOT_TOKEN = os.environ.get("BOT_TOKEN", None)
 SUPPORT = os.environ.get("SUPPORT", None)
 UPDATES = os.environ.get("UPDATES", None)
 BOT_USERNAME = os.environ.get("BOT_USERNAME", None) 
+PM_START_IMG = os.environ.get("PM_START_IMG" , "https://telegra.ph/file/895f87f9f8028ecc4b632.jpg")
 
 bot = Client("nandhabot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN, plugins=dict(root="{}/plugins".format(__name__)))
 
 
 PM_START_TEXT = "hello! {}\nI'm anime themed nekos bot below click the help button know my commandslist!"
-PM_START_IMG = [
- "https://telegra.ph/file/5d0953a536678e88e9e2a.jpg",
-"https://telegra.ph/file/ce2fda9ecae0a574f1d42.jpg",
-"https://telegra.ph/file/ee4087f9c29b16e6a1b64.jpg",
-"https://telegra.ph/file/529c6071dbdd7600990ad.jpg" ]
-
